5694afd6a687f660fbba65a908d6ff0f1b4265ed
irixa.vales <irixa.vales@INCADELWS14.ecenet.ece.uprm.edu>
2017-03-17 11:30:52 -0400

    fixed

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -10,7 +10,7 @@ import javax.swing.JFrame;
 public class MyMouseAdapter extends MouseAdapter {
 	private Color existingColor;
 	private Color[] existingColorArray;
-	
+
 	private int existingCaseNum;
 	private int[] existingCaseNumbers = new int[Math.max(MyPanel.TOTAL_COLUMNS, MyPanel.TOTAL_ROWS)];
 	private int[][] existingCaseNumbers2 = new int[3][3];
@@ -116,47 +116,47 @@ public class MyMouseAdapter extends MouseAdapter {
 							}
 							myPanel.repaint();
 						}
-						
+
 						else if (gridX == 0 && gridY == MyPanel.TOTAL_ROWS - 1) {
 							//On the bottom-left cell
 							for (int i = 0; i < 3; i++) {
 								for (int j = 0; j<3; j++) {
-									
-								
-								Color newColor = null;
-								int caseNum = generator.nextInt(5);
-								if (!myPanel.colorArray[4+i][4+j].equals(Color.WHITE)) {
-									while (caseNum == existingCaseNumbers2[i][j]) {
-										caseNum = generator.nextInt(5);
+
+
+									Color newColor = null;
+									int caseNum = generator.nextInt(5);
+									if (!myPanel.colorArray[4+i][4+j].equals(Color.WHITE)) {
+										while (caseNum == existingCaseNumbers2[i][j]) {
+											caseNum = generator.nextInt(5);
+										}
 									}
+									switch (caseNum) 
+									{
+									case 0:
+										newColor = Color.YELLOW;
+										break;
+									case 1:
+										newColor = Color.MAGENTA;
+										break;
+									case 2:
+										newColor = Color.BLACK;
+										break;
+									case 3:
+										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									case 4:
+										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									}
+									existingCaseNumbers2[i][j] = caseNum;
+									myPanel.colorArray[4+i][4+j] = newColor;
 								}
-								switch (caseNum) 
-								{
-								case 0:
-									newColor = Color.YELLOW;
-									break;
-								case 1:
-									newColor = Color.MAGENTA;
-									break;
-								case 2:
-									newColor = Color.BLACK;
-									break;
-								case 3:
-									newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								case 4:
-									newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								}
-								existingCaseNumbers2[i][j] = caseNum;
-								myPanel.colorArray[4+i][4+j] = newColor;
+								myPanel.repaint();
 							}
-							myPanel.repaint();
 						}
-						}
-						
+
 						else if (gridX == 0) {
-							
+
 							//On the left column
 							Color newColor = null;
 							for (int i = 1; i < MyPanel.TOTAL_COLUMNS; i++) {
@@ -180,8 +180,9 @@ public class MyMouseAdapter extends MouseAdapter {
 											newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
 											break;
 										}
-									} while (newColorArray.equals(existingColorArray[i]));
+									} while (newColor.equals(existingColorArray[i]));
 								}
+
 								else {
 									switch (generator.nextInt(5)) 
 									{
@@ -205,9 +206,9 @@ public class MyMouseAdapter extends MouseAdapter {
 								existingColorArray[i] = newColor;
 								myPanel.colorArray[gridX][gridY] = newColor;
 								myPanel.repaint();
+							}
 						}
-						}
-						
+
 						else if (gridY == 0) {
 							//On the top row
 							for (int j = 1; j < MyPanel.TOTAL_ROWS; j++) {
@@ -241,60 +242,38 @@ public class MyMouseAdapter extends MouseAdapter {
 							}
 							myPanel.repaint();
 						}
-						
+
 						else {
 							//On the grid other than on the left column and on the top row:
-						Color newColor = null;
-						if (!myPanel.colorArray[gridX][gridY].equals(Color.WHITE)) {
-							do {
-								switch (generator.nextInt(5)) 
-								{
-								case 0:
-									newColor = Color.YELLOW;
-									break;
-								case 1:
-									newColor = Color.MAGENTA;
-									break;
-								case 2:
-									newColor = Color.BLACK;
-									break;
-								case 3:
-									newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								case 4:
-									newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								}
-							} while (newColor.equals(existingColor));
+							Color newColor = null;
+								do {
+									switch (generator.nextInt(5)) 
+									{
+									case 0:
+										newColor = Color.YELLOW;
+										break;
+									case 1:
+										newColor = Color.MAGENTA;
+										break;
+									case 2:
+										newColor = Color.BLACK;
+										break;
+									case 3:
+										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									case 4:
+										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									}
+								} while (newColor.equals(existingColor));
+							existingColor = newColor;
+							myPanel.colorArray[gridX][gridY] = newColor;
+							myPanel.repaint();
 						}
-						else {
-							switch (generator.nextInt(5)) 
-							{
-							case 0:
-								newColor = Color.YELLOW;
-								break;
-							case 1:
-								newColor = Color.MAGENTA;
-								break;
-							case 2:
-								newColor = Color.BLACK;
-								break;
-							case 3:
-								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							case 4:
-								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							}
-						}
-						existingColor = newColor;
-						myPanel.colorArray[gridX][gridY] = newColor;
-						myPanel.repaint();
 					}
 				}
-			}
-			myPanel.repaint();
-			break;
+				myPanel.repaint();
+				break;}
 		case 3:		//Right mouse button
 			//Do nothing
 			break;
