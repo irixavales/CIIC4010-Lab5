cd10e2e8ee9b832dcd0caef47525a0c0064ec843
3010a17 <3010a17@guayaba.ece.uprm.edu>
2017-03-17 14:05:20 -0400

    finished

--- Lab5/src/MyMouseAdapter.java
+++ Lab5/src/MyMouseAdapter.java
@@ -8,12 +8,9 @@ import java.util.Random;
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private Color existingColor;
-	private Color[] existingColorArray;
 
-	private int existingCaseNum;
-	private int[] existingCaseNumbers = new int[Math.max(MyPanel.TOTAL_COLUMNS, MyPanel.TOTAL_ROWS)];
-	private int[][] existingCaseNumbers2 = new int[3][3];
+	private Color existingColor;
+	private Color[] existingColorArray = new Color[Math.max(MyPanel.TOTAL_COLUMNS, MyPanel.TOTAL_ROWS)];
 
 	private Random generator = new Random();
 	public void mousePressed(MouseEvent e) {
@@ -84,168 +81,11 @@ public class MyMouseAdapter extends MouseAdapter {
 					} else {
 						//Released the mouse button on the same cell where it was pressed
 						if (gridX == 0 && gridY == 0) {
+
 							//On the top-left cell
-							for (int i = 1; i < MyPanel.TOTAL_COLUMNS; i++) {
-								Color newColor = null;
-								int caseNum = generator.nextInt(5);
-								if (!myPanel.colorArray[i][i].equals(Color.WHITE)) {
-									while (caseNum == existingCaseNumbers[i-1]) {
-										caseNum = generator.nextInt(5);
-									}
-								}
-								switch (caseNum) 
-								{
-								case 0:
-									newColor = Color.YELLOW;
-									break;
-								case 1:
-									newColor = Color.MAGENTA;
-									break;
-								case 2:
-									newColor = Color.BLACK;
-									break;
-								case 3:
-									newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								case 4:
-									newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								}
-								existingCaseNumbers[i-1] = caseNum;
-								myPanel.colorArray[i][i] = newColor;
-							}
-							myPanel.repaint();
-						}
 
-						else if (gridX == 0 && gridY == MyPanel.TOTAL_ROWS - 1) {
-							//On the bottom-left cell
-							for (int i = 0; i < 3; i++) {
-								for (int j = 0; j<3; j++) {
-
-
-									Color newColor = null;
-									int caseNum = generator.nextInt(5);
-									if (!myPanel.colorArray[4+i][4+j].equals(Color.WHITE)) {
-										while (caseNum == existingCaseNumbers2[i][j]) {
-											caseNum = generator.nextInt(5);
-										}
-									}
-									switch (caseNum) 
-									{
-									case 0:
-										newColor = Color.YELLOW;
-										break;
-									case 1:
-										newColor = Color.MAGENTA;
-										break;
-									case 2:
-										newColor = Color.BLACK;
-										break;
-									case 3:
-										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									case 4:
-										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									}
-									existingCaseNumbers2[i][j] = caseNum;
-									myPanel.colorArray[4+i][4+j] = newColor;
-								}
-								myPanel.repaint();
-							}
-						}
-
-						else if (gridX == 0) {
-
-							//On the left column
 							Color newColor = null;
-							for (int i = 1; i < MyPanel.TOTAL_COLUMNS; i++) {
-								if (!myPanel.colorArray[gridX][gridY].equals(Color.WHITE)) {
-									do {
-										switch (generator.nextInt(5)) 
-										{
-										case 0:
-											newColor = Color.YELLOW;
-											break;
-										case 1:
-											newColor = Color.MAGENTA;
-											break;
-										case 2:
-											newColor = Color.BLACK;
-											break;
-										case 3:
-											newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-											break;
-										case 4:
-											newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-											break;
-										}
-									} while (newColor.equals(existingColorArray[i]));
-								}
-
-								else {
-									switch (generator.nextInt(5)) 
-									{
-									case 0:
-										newColor = Color.YELLOW;
-										break;
-									case 1:
-										newColor = Color.MAGENTA;
-										break;
-									case 2:
-										newColor = Color.BLACK;
-										break;
-									case 3:
-										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									case 4:
-										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-										break;
-									}
-								}
-								existingColorArray[i] = newColor;
-								myPanel.colorArray[gridX][gridY] = newColor;
-								myPanel.repaint();
-							}
-						}
-
-						else if (gridY == 0) {
-							//On the top row
-							for (int j = 1; j < MyPanel.TOTAL_ROWS; j++) {
-								Color newColor = null;
-								int caseNum = generator.nextInt(5);
-								if (!myPanel.colorArray[gridX][j].equals(Color.WHITE)) {
-									while (caseNum == existingCaseNumbers[j-1]) {
-										caseNum = generator.nextInt(5);
-									}
-								}
-								switch (caseNum) 
-								{
-								case 0:
-									newColor = Color.YELLOW;
-									break;
-								case 1:
-									newColor = Color.MAGENTA;
-									break;
-								case 2:
-									newColor = Color.BLACK;
-									break;
-								case 3:
-									newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								case 4:
-									newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-									break;
-								}
-								existingCaseNumbers[j-1] = caseNum;
-								myPanel.colorArray[gridX][j] = newColor;
-							}
-							myPanel.repaint();
-						}
-
-						else {
-							//On the grid other than on the left column and on the top row:
-							Color newColor = null;
+							for (int i = 1; i < Math.max(MyPanel.TOTAL_COLUMNS, MyPanel.TOTAL_ROWS); i++) {
 								do {
 									switch (generator.nextInt(5)) 
 									{
@@ -265,7 +105,141 @@ public class MyMouseAdapter extends MouseAdapter {
 										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
 										break;
 									}
-								} while (newColor.equals(existingColor));
+								} while (newColor.equals(existingColorArray[i]));
+								existingColorArray[i] = newColor;
+								myPanel.colorArray[i][i] = newColor;
+								myPanel.repaint();
+							}
+						}
+
+						else if (gridX == 0 && gridY == MyPanel.TOTAL_ROWS - 1) {
+
+							//On the bottom-left cell
+
+							for (int i = 0; i < 3; i++) {
+								for (int j = 0; j<3; j++) {
+
+
+									Color newColor = null;
+									
+										do {
+											switch (generator.nextInt(5)) 
+											{
+											case 0:
+												newColor = Color.YELLOW;
+												break;
+											case 1:
+												newColor = Color.MAGENTA;
+												break;
+											case 2:
+												newColor = Color.BLACK;
+												break;
+											case 3:
+												newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+												break;
+											case 4:
+												newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+												break;
+											}
+										} while (newColor.equals(existingColorArray[i]));
+										existingColorArray[i] = newColor;
+										myPanel.colorArray[(MyPanel.TOTAL_COLUMNS - 2)/2 + i][(MyPanel.TOTAL_ROWS - 3)/2 + j] = newColor;
+										myPanel.repaint();
+									}
+									myPanel.repaint();
+								}
+							}
+						
+
+						else if (gridX == 0) {
+
+							//On the left column
+
+							Color newColor = null;
+							for (int i = 1; i < MyPanel.TOTAL_COLUMNS; i++) {
+								do {
+									switch (generator.nextInt(5)) 
+									{
+									case 0:
+										newColor = Color.YELLOW;
+										break;
+									case 1:
+										newColor = Color.MAGENTA;
+										break;
+									case 2:
+										newColor = Color.BLACK;
+										break;
+									case 3:
+										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									case 4:
+										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									}
+								} while (newColor.equals(existingColorArray[i]));
+								existingColorArray[i] = newColor;
+								myPanel.colorArray[i][gridY] = newColor;
+								myPanel.repaint();
+							}
+						}
+
+						else if (gridY == 0) {
+
+							//On the top row
+
+							Color newColor = null;
+							for (int i = 1; i < MyPanel.TOTAL_ROWS; i++) {
+								do {
+									switch (generator.nextInt(5)) 
+									{
+									case 0:
+										newColor = Color.YELLOW;
+										break;
+									case 1:
+										newColor = Color.MAGENTA;
+										break;
+									case 2:
+										newColor = Color.BLACK;
+										break;
+									case 3:
+										newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									case 4:
+										newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+										break;
+									}
+								} while (newColor.equals(existingColorArray[i]));
+								existingColorArray[i] = newColor;
+								myPanel.colorArray[gridX][i] = newColor;
+								myPanel.repaint();
+							}
+						}
+
+						else {
+
+							//On the grid other than on the left column and on the top row
+
+							Color newColor = null;
+							do {
+								switch (generator.nextInt(5)) 
+								{
+								case 0:
+									newColor = Color.YELLOW;
+									break;
+								case 1:
+									newColor = Color.MAGENTA;
+									break;
+								case 2:
+									newColor = Color.BLACK;
+									break;
+								case 3:
+									newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+									break;
+								case 4:
+									newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+									break;
+								}
+							} while (newColor.equals(existingColor));
 							existingColor = newColor;
 							myPanel.colorArray[gridX][gridY] = newColor;
 							myPanel.repaint();
@@ -273,10 +247,35 @@ public class MyMouseAdapter extends MouseAdapter {
 					}
 				}
 				myPanel.repaint();
-				break;}
+				break;
+			}
+			
 		case 3:		//Right mouse button
-			//Do nothing
+			
+			c = e.getComponent();
+			while (!(c instanceof JFrame)) {
+				c = c.getParent();
+				if (c == null) {
+					return;
+				}
+			}
+			myFrame = (JFrame)c;
+			myPanel = (MyPanel) myFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+			for (int i=1; i < MyPanel.TOTAL_COLUMNS; i++) {
+				for (int j=1; j < MyPanel.TOTAL_ROWS; j++) {
+					if(!MyPanel.colorArray[i][j].equals(Color.WHITE)) {
+						float r = generator.nextFloat();
+						float g = generator.nextFloat();
+						float b = generator.nextFloat();
+						Color newColor = new Color(r, g, b);
+						MyPanel.colorArray[i][j] = newColor;
+						myPanel.repaint();
+					}
+				}
+			}
+			
 			break;
+			
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
--- Lab5/src/MyPanel.java
+++ Lab5/src/MyPanel.java
@@ -16,7 +16,7 @@ public class MyPanel extends JPanel {
 	public int y = -1;
 	public int mouseDownGridX = 0;
 	public int mouseDownGridY = 0;
-	public Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
+	public static Color[][] colorArray = new Color[TOTAL_COLUMNS][TOTAL_ROWS];
 	public MyPanel() {   //This is the constructor... this code runs first to initialize
 		if (INNER_CELL_SIZE + (new Random()).nextInt(1) < 1) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("INNER_CELL_SIZE must be positive!");
